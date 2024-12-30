0534493790da9597576dea089d556950a180f9df
Ían Muliterno <iannimuliterno@gmail.com>
2024-12-29 22:10:45 -0300

    create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup 
+  
+
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+setup( 
+    name='Book Recommendation', 
+    version='0.1', 
+    description='A personal project', 
+    author='Ian Muliterno', 
+    author_email='ianmuliterno@gmail.com', 
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+) 
